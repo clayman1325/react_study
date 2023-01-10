151c31cd808723008bfb7c0cb0868c79bc377299
davidfdomejiab <davidfdomejiab@gmail.com>
2023-01-10 17:54:30 -0500

    section 16 adding validation to simple form before refactor

--- section_16/01-starting-project/src/components/SimpleInput.js
+++ section_16/01-starting-project/src/components/SimpleInput.js
@@ -1,9 +1,51 @@
+import { useState, useRef, useEffect } from "react";
+
 const SimpleInput = (props) => {
+  const nameRef = useRef();
+  const [name, setName] = useState("");
+  const [nameIsValid, setNameIsValid] = useState(true)
+
+  const nameHandler = (event) => {
+    setName(event.target.value)
+  }
+
+  const formSubmisionHandler = (event) => {
+    event.preventDefault();
+    console.log(name)
+    console.log(nameRef.current.value)
+    if(name.trim() == 0) {
+      setNameIsValid(false)
+      return;
+    }
+    setNameIsValid(true);
+    setName("")
+  }
+
+  const onBlurHandler = () => {
+    if(name.trim() == 0) {
+      setNameIsValid(false)
+      return;
+    }
+  }
+
+  const onKeyDownHandler = (event) => {
+    if(event.target.value.trim() != 0) {
+      setNameIsValid(true)
+    }
+  }
+
+  const nameInputCLases = nameIsValid ? "form-control" : "form-control invalid"
+
+  useEffect(() =>{
+    if(nameIsValid) console.log("yei")
+  },[nameIsValid])
+
   return (
-    <form>
-      <div className='form-control'>
+    <form onSubmit={formSubmisionHandler}>
+      <div className={nameInputCLases}>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input className={nameInputCLases} ref={nameRef} type='text' id='name' value={name} onChange={nameHandler} onBlur={onBlurHandler} onKeyUp={onKeyDownHandler}/>
+        { !nameIsValid && <p className="error-text"> please check that the name is correct </p>}
       </div>
       <div className="form-actions">
         <button>Submit</button>
