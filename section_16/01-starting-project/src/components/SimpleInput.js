b7dcae7f81838eb59d39146863184d9d44598b58
davidfdomejiab <davidfdomejiab@gmail.com>
2023-01-10 18:08:03 -0500

    section 16 refactor the validation business logic  of the simple form

--- section_16/01-starting-project/src/components/SimpleInput.js
+++ section_16/01-starting-project/src/components/SimpleInput.js
@@ -3,35 +3,25 @@ import { useState, useRef, useEffect } from "react";
 const SimpleInput = (props) => {
   const nameRef = useRef();
   const [name, setName] = useState("");
-  const [nameIsValid, setNameIsValid] = useState(true)
+  const [nameActivated, setNameActivated] = useState(false)
+
+  const nameIsValid = !nameActivated || name.trim() != 0 && nameActivated;
 
   const nameHandler = (event) => {
     setName(event.target.value)
+    setNameActivated(true)
   }
-
+  const onBlurHandler = () => {
+    setNameActivated(true)
+  }
   const formSubmisionHandler = (event) => {
     event.preventDefault();
-    console.log(name)
-    console.log(nameRef.current.value)
-    if(name.trim() == 0) {
-      setNameIsValid(false)
+
+    if(!nameIsValid) {
       return;
     }
-    setNameIsValid(true);
     setName("")
-  }
-
-  const onBlurHandler = () => {
-    if(name.trim() == 0) {
-      setNameIsValid(false)
-      return;
-    }
-  }
-
-  const onKeyDownHandler = (event) => {
-    if(event.target.value.trim() != 0) {
-      setNameIsValid(true)
-    }
+    setNameActivated(false)
   }
 
   const nameInputCLases = nameIsValid ? "form-control" : "form-control invalid"
@@ -44,7 +34,7 @@ const SimpleInput = (props) => {
     <form onSubmit={formSubmisionHandler}>
       <div className={nameInputCLases}>
         <label htmlFor='name'>Your Name</label>
-        <input className={nameInputCLases} ref={nameRef} type='text' id='name' value={name} onChange={nameHandler} onBlur={onBlurHandler} onKeyUp={onKeyDownHandler}/>
+        <input className={nameInputCLases} ref={nameRef} type='text' id='name' value={name} onChange={nameHandler} onBlur={onBlurHandler}/>
         { !nameIsValid && <p className="error-text"> please check that the name is correct </p>}
       </div>
       <div className="form-actions">
