4766598309379b834889858d7ec52b591fdd6a80
David Mejia <davidfdomejiab@gmail.com>
2023-08-26 21:33:19 -0500

    create a web-server using the popular express package

--- node_study/nodejs_api/3.express.js
+++ node_study/nodejs_api/3.express.js
@@ -1,22 +1,18 @@
 const express = require('express')
 const ditto = require('./pokemon/ditto.json')
-const path = require('path')
 
 const PORT = process.env.PORT ?? 1234
 
 const app = express()
 app.disable('x-powered-by')
+app.use(express.json)
 
-app.use(express.json())
-
+// # validation example
 // app.use((req, res, next) => {
 //   if (req.method !== 'POST') return next()
 //   if (req.headers['content-type'] !== 'application/json') return next()
 
-//   // solo llegan request que son POST y que tienen el header Content-Type: application/json
 //   let body = ''
-
-//   // escuchar el evento data
 //   req.on('data', chunk => {
 //     body += chunk.toString()
 //   })
@@ -24,26 +20,31 @@ app.use(express.json())
 //   req.on('end', () => {
 //     const data = JSON.parse(body)
 //     data.timestamp = Date.now()
-//     // mutar la request y meter la información en el req.body
 //     req.body = data
 //     next()
 //   })
 // })
 
-app.get('/pokemon/ditto', (req, res) => {
-  res.json(ditto)
+const url = '/pokemon/ditto'
+const callback = (req, res) => {
+    res.json(ditto)
+}
+app.get(url, callback)
+app.get(url, (req, res) => {
+    console.log("get in")
+    res.status(200).json({"hello": "world"})
 })
 
 app.post('/pokemon', (req, res) => {
-  // req.body deberíamos guardar en bbdd
-  res.status(201).json(req.body)
+    res.status(201).json(req.body)
 })
 
-// la última a la que va a llegar
 app.use((req, res) => {
-  res.status(404).send('<h1>404</h1>')
+    res.status(404).send('<h1>404</h1>')
 })
 
 app.listen(PORT, () => {
-  console.log(`server listening on port http://localhost:${PORT}`)
+    console.log(`server listening on port http://localhost:${PORT}`)
 })
+
+
