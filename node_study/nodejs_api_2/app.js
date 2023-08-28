6d6c687888e3bf77ab1a1ef8cf5bd9591c98116b
David Mejia <davidfdomejiab@gmail.com>
2023-08-28 10:25:58 -0500

    incorporate validations and a basic crud for movies api

---
+++ node_study/nodejs_api_2/app.js
@@ -0,0 +1,108 @@
+const express = require('express')
+const crypto = require('node:crypto')
+const cors = require('cors')
+const movies = require('./movies.json')
+const { validateMovie, validatePartialMovie } = require('./schemas/movies')
+const { resourceLimits } = require('node:worker_threads')
+const html = require("./web/index.html")
+
+const app = express()
+app.use(express.json())
+app.use(cors({
+    origin: (origin, callback) => {
+      const ACCEPTED_ORIGINS = [
+        'http://localhost:8080',
+        'http://localhost:1234',
+        'https://movies.com',
+      ]
+
+      if (ACCEPTED_ORIGINS.includes(origin)) {
+        return callback(null, true)
+      }
+
+      if (!origin) {
+        return callback(null, true)
+      }
+
+      return callback(new Error('Not allowed by CORS'))
+    }
+}))
+
+app.get("/", (req,res) => {
+    res.status(200).send(html)
+})
+
+app.get("/movies",(req, res) => {
+    const { genre } = req.query
+    if (genre) {
+      const filteredMovies = movies.filter(
+        movie => movie.genre.some(g => g.toLowerCase() === genre.toLowerCase())
+      )
+      return res.json(filteredMovies)
+    }
+    res.json(movies)
+})
+
+app.get("/movies/:id", (req, res) => {
+    const { id } = req.params
+    const movie = movies.find(movie => movie.id == id)
+    if(movie) return res.json(movie)
+    res.status(404).json({ message: "Movie not found"})
+})
+
+app.post("/movies", (req, res) => {
+    const result = validateMovie(req.body)
+    if(!result.success) {
+        return res.status(400).json({ error: JSON.parse(result.error.message) })
+    }
+
+    const newMovie = {
+        id: crypto.randomUUID,
+        ...result.data
+    }
+    movies.push(newMovie)
+    res.status(201).json(newMovie)
+})
+
+app.patch("/movies/:id", (req, res) => {
+    const result = validatePartialMovie(req.body)
+
+    if(!result.success) {
+        return res.status(400).json({ error: JSON.parse(result.error.message) })
+    }
+
+    const { id } = req.params
+    const movieIndex = movies.findIndex(movie => movie.id === id)
+
+    if (movieIndex === -1) {
+        return res.status(404).json({ message: 'Movie not found' })
+    }
+
+    const updateMovie = {
+        ...movies[index],
+        ...result.data
+    }
+
+    movies[movieIndex] = updateMovie
+
+    return res.json(updateMovie)
+})
+
+app.delete('/movies/:id', (req, res) => {
+    const { id } = req.params
+    const movieIndex = movies.findIndex(movie => movie.id === id)
+
+    if (movieIndex === -1) {
+      return res.status(404).json({ message: 'Movie not found' })
+    }
+
+    movies.splice(movieIndex, 1)
+
+    return res.json({ message: 'Movie deleted' })
+})
+
+const PORT = process.env.PORT ?? 1234
+
+app.listen(PORT, () => {
+  console.log(`server listening on port http://localhost:${PORT}`)
+})
