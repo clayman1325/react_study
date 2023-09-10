03a02009c720eac903719fd84142411b33d692eb
David Mejia <davidfdomejiab@gmail.com>
2023-09-09 19:34:53 -0500

    test api with local data

--- node_study/nodejs_api_3/app.js
+++ node_study/nodejs_api_3/app.js
@@ -1,16 +1,19 @@
 import express, { json } from "express"
 import { createMovieRouter } from "./routes/movies.js"
-import { CorsMidelware } from "./middlewares/cors.js"
+import { corsMiddleware } from "./middlewares/cors.js"
 
 import "dotenv/config"
 
 export const createApp = ({ movieModel }) => {
     const app = express()
     app.use(json())
-    app.use(CorsMidelware)
+    app.use(corsMiddleware)
     app.use("/movies", createMovieRouter({ movieModel }))
-
-    const PORT = process.env.PORT ?? 3001
+    app.get('/', (req, res) => {
+        console.log("test. ..... . .. . . ..")
+        res.json({"message": "success"})
+    })
+    const PORT = process.env.PORT ?? 3000
 
     app.listen(PORT, () => {
         console.log(`server listening on port http://localhost:${PORT}`)
